6db069823dd7f6cc6e96852c4f6cfee9292ff487
njorokel <kelvinmkoloni17@gmail.com>
2023-03-06 15:32:03 +0300

    my python script using if

---
+++ app.py
@@ -0,0 +1,9 @@
+outcome=input("enter outcome\n")
+if outcome=='E':
+    print("exceed expectation")
+if outcome=="M":
+    print("met expectation")
+if outcome=="A":
+    print("approaching expectation")
+else:
+    print("below expectation")     
